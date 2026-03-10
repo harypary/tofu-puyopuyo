a44f4b194e1e4d82f3507bbe525d818d6b85b6e5
TofuGame <build@tofugame.com>
2026-03-10 21:30:57 +0900

    Set iOS App Store icon (1024x1024) programmatically in BuildScript

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 using System;
 
 public class BuildScript
@@ -9,6 +10,17 @@ public class BuildScript
         PlayerSettings.applicationIdentifier = "com.harypary.tofupuyopuyo";
         PlayerSettings.bundleVersion = "1.0";
         PlayerSettings.iOS.buildNumber = Environment.GetEnvironmentVariable("BUILD_NUMBER") ?? "1";
+
+        // Set 1024x1024 App Store icon
+        var icon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/AppIcon.png");
+        if (icon != null)
+        {
+            var kind = UnityEditor.iOS.iOSPlatformIconKind.Application;
+            var icons = PlayerSettings.GetPlatformIcons(BuildTargetGroup.iOS, kind);
+            for (int i = 0; i < icons.Length; i++)
+                icons[i].SetTexture(icon);
+            PlayerSettings.SetPlatformIcons(BuildTargetGroup.iOS, kind, icons);
+        }
 
         var options = new BuildPlayerOptions
         {
